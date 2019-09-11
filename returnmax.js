1866fcaf30371d54dd9bb524b687d54ee32240eb
Ramesh Chuwan <rameshchuwan19@gmail.com>
2019-09-11 14:17:26 -0500

    Return the maxium value

---
+++ returnmax.js
@@ -0,0 +1,11 @@
+//return the max number
+
+function max(arr) {
+    var max = arr[0]
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] > max) {
+            max = arr[i]
+        }
+    }
+    return max
+}
