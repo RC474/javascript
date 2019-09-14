8f4759f97a1c21cfda30724c666f6e813b540705
Ramesh Chuwan <rameshchuwan19@gmail.com>
2019-09-14 13:07:48 -0500

    Return The Max Number Commit

--- returnmax.js
+++ returnmax.js
@@ -9,3 +9,103 @@ function max(arr) {
     }
     return max
 }
+
+
+/* function max(arr) {
+  var max = arr[0]; 
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] > max) {
+       max = arr[i];
+    }
+   
+  }
+  return max;
+}
+
+max([10,40,7]);
+
+
+
+ 
+
+var colors = ["red", "green", "blue", "white", "pink"]
+ */
+/* function dispalyColors (arr) {
+  var newcolors = [];
+  for(let i = 0; i < arr.length; i++){
+    newcolors.push(arr[i])
+  }
+  return newcolors;
+}
+dispalyColors(colors);
+
+ */
+ /* var counter = 0;
+while(counter < colors.length) {
+  
+  console.log(counter + 1 + "." + colors[counter])
+
+  counter++;
+}  */
+/* var newCol
+for(let i = 0; i < colors.length; i++) {
+ newCol = i + 1 + "." + colors[i] 
+ console.log(newCol);
+}
+
+ */
+
+ /* var counter = 0;
+while(counter <= 100) {
+  if(counter % 2 !== 0){
+    console.log(counter)
+    
+  }
+  counter++
+  
+}
+    */
+
+  /* for(let i = 0; i < 101; i++) {
+    if(i % 2 === 0){
+      console.log(i)
+    }
+  } */
+
+  /*  for(let i = 97; i <= 122; i++){
+    console.log(i + "." +String.fromCharCode(i))
+
+  } */
+
+  
+ 
+  
+
+ /* function randomPass() {
+  var ch1 = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  var ch2 = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  var ch3 = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  var ch4 = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  var ch5 = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  var ch6 = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  var ch7 = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  var ch8 = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  
+  
+  var full = ch1 + ch2 + ch3 + ch4 + ch5 + ch6 + ch7 + ch8;
+  
+  return full;
+
+}; 
+ */
+/* randomPass();
+
+
+for(let i = 0; i <= 100; i++) {
+  if(i % 2 === 0){
+    console.log(i);
+  };
+};
+
+Math.floor(Math.random() * 60) + 20; */
+
